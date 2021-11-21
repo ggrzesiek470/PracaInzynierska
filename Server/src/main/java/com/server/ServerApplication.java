981218a11d7bbf56faa5ccc1bc14eb77a100928e
ggrzesiek470 <grzegorczykgrzegorz123@gmail.com>
2021-11-21 22:57:17 +0100

    grzegorz -ia-module-configured

---
+++ Server/src/main/java/com/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ServerApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "3001"));
+		app.run(args);;
+	}
+}
